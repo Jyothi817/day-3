ea0e2903adc86d82d22dbbb52a35e525739268a6
Jyothi817 <jyothivijay817@gmail.com>
2026-07-24 13:03:07 +0530

    shopping_bill.py

---
+++ shopping_bill.py
@@ -0,0 +1,10 @@
+print("="*25)
+print("SHOPPING BILL")
+print("Item ", "QTY", "Price", sep="\t")
+print("shoes", "2 pairs", "Rs.1000", sep="\t")
+print("Watch ", "1", "Rs.2000", sep="\t")
+print("Sarees","3", "Rs.15000", sep="\t")
+print("="*25)
+print("Total", "Rs.18000", sep="\t")
+
+
